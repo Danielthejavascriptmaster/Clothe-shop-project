0cb72e5c0757638747a8d3d677bccf00df9384c6
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-02-24 20:00:58 +0100

    ralph lauren detail update 2

--- Product detials/ralph lauren/Ralplauren.js
+++ Product detials/ralph lauren/Ralplauren.js
@@ -3,3 +3,44 @@ const beigeBtn = document.getElementById("beigeBtn");
 const whiteBtn = document.getElementById("whiteBtn");
 const productImage = document.getElementById("productImage");
 
+const rightArrow = document.querySelector(".rightArrow");
+const leftArrow = document.querySelector(".leftArrow");
+
+
+
+rightArrow.addEventListener("click", function(){
+    if(whiteBtn.classList.contains("active"))
+    productImage.src = "/pictures/ralph lauren cable knit sweater white backside.jpg"
+});
+
+
+
+leftArrow.addEventListener("click", function(){
+    if(whiteBtn.classList.contains("active"))
+    productImage.src = "/pictures/ralph lauren cable knit sweater white.jpg";
+});
+
+
+
+function resetActive(){
+    beigeBtn.classList.remove("active");
+    whiteBtn.classList.remove("active");
+    blueBtn.classList.remove("active");
+};
+
+
+
+whiteBtn.addEventListener("click", function(){
+    resetActive();
+    whiteBtn.classList.add("active");
+    productImage.src = "/pictures/ralph lauren cable knit sweater white.jpg"
+});
+
+
+
+beigeBtn.addEventListener("click", function(){
+    resetActive();
+    beigeBtn.classList.add("active");
+    productImage.src = "/pictures/ralph lauren cable knit sweater beige.jpg"
+});
+
