5ad7a933a997dcd657ffa66987d09db6af8369d2
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-02-24 19:30:35 +0100

    collection + js added

---
+++ Product detials/ralph lauren/Ralplauren.js
@@ -0,0 +1,5 @@
+const blueBtn = document.getElementById("blueBtn");
+const beigeBtn = document.getElementById("beigeBtn");
+const whiteBtn = document.getElementById("whiteBtn");
+const productImage = document.getElementById("productImage");
+
