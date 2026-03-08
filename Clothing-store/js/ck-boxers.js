1e4743513b271ec1bf3fb0044d9d7b41f6c9ca1b
Danielthejavascriptmaster <onewhoknocksthe0.com>
2026-03-08 10:55:06 +0100

    product update 10

--- Clothing-store/js/ck-boxers.js
+++ Clothing-store/js/ck-boxers.js
@@ -26,6 +26,68 @@ cartBtn.addEventListener("click", function(){
         money = money - 1;
     }
 
-     
-    
-})
+});
+
+window.onload = function(){
+        const sizeS = document.getElementById("S");
+        const sizeM = document.getElementById("M");
+        const sizeL = document.getElementById("L");
+        const sizeXL = document.getElementById("XL")
+
+        sizeS.addEventListener("click", function(){
+           sizeS.style.background = "white";
+            sizeS.style.color = "black";
+
+            sizeM.style.background = "black";
+            sizeM.style.color = "white"
+
+            sizeL.style.background = "black"
+            sizeL.style.color = "white"
+
+            
+            sizeXL.style.background = "black"
+            sizeXL.style.color = "white"
+        });
+
+        sizeM.addEventListener("click", function(){
+           sizeM.style.background = "white";
+            sizeM.style.color = "black";
+
+            sizeS.style.background = "black";
+            sizeS.style.color = "white"
+
+            sizeL.style.background = "black"
+            sizeL.style.color = "white"
+
+            sizeXL.style.background = "black"
+            sizeXL.style.color = "white"
+        });
+
+        sizeL.addEventListener("click", function(){
+           sizeL.style.background = "white";
+            sizeL.style.color = "black";
+
+            sizeS.style.background = "black";
+            sizeS.style.color = "white"
+
+            sizeM.style.background = "black"
+            sizeM.style.color = "white"
+
+            sizeXL.style.background = "black"
+            sizeXL.style.color = "white"
+        });
+
+        sizeXL.addEventListener("click", function(){
+            sizeXL.style.background = "white";
+            sizeXL.style.color = "black";
+
+            sizeS.style.background = "black";
+            sizeS.style.color = "white"
+
+            sizeM.style.background = "black"
+            sizeM.style.color = "white"
+
+            sizeL.style.background = "black"
+            sizeL.style.color = "white"
+        })
+}
--- Clothing-store/js/true-religion.js
+++ Clothing-store/js/true-religion.js
@@ -1,79 +1,3 @@
-const blackBtn = document.getElementById("blackBtn");
-const whiteBtn = document.getElementById("whiteBtn");
-const greyBtn = document.getElementById("greyBtn");
-const beigeBtn = document.getElementById("beigeBtn")
-const blueBtn = document.getElementById("blueBtn")
-const productImage = document.getElementById("productImage");
-
-function resetActive(){
-    blackBtn.classList.remove("active");
-    whiteBtn.classList.remove("active");
-    greyBtn.classList.remove("active");
-    blueBtn.classList.remove("active");
-    beigeBtn.classList.remove("active");
-};
-
-whiteBtn.addEventListener("click", function(){
-    resetActive();
-    whiteBtn.classList.add("active");
-    productImage.src = "/Clothing-store/assets/images/white t shirt.webp"
-})
-
-beigeBtn.addEventListener("click", function(){
-    resetActive();
-    beigeBtn.classList.add("active");
-    productImage.src = "/Clothing-store/assets/images/beige t shirt.jpg"
-});
-
-blueBtn.addEventListener("click", function(){
-    resetActive();
-    blueBtn.classList.add("active");
-    productImage.src = "/Clothing-store/assets/images/navy blue t shirt.avif"
-});
-
-greyBtn.addEventListener("click", function(){
-    resetActive();
-    greyBtn.classList.add("active");
-    productImage.src = "/Clothing-store/assets/images/grey t shirt.jpg"
-});
-
-blackBtn.addEventListener("click", function(){
-    resetActive();
-    blackBtn.classList.add("active");
-    productImage.src = "/Clothing-store/assets/images/black t shirt.jpg"
-});
-
-const cartBtn = document.querySelector(".cartBtn");
-const jacketPrice = 5;
-let money = 700;
-const balanceDisplay = document.getElementById("balanceContainer");
-
-const thankYouPopup = document.getElementById("thankYouPopup");
-
-cartBtn.addEventListener("click", function(){
-    money -= jacketPrice;
-     window.alert("Item added to cart!")
-
-    balanceDisplay.textContent = "Balance: £" + money.toFixed(2);
-
-
-    thankYouPopup.style.opacity = 1;
-
-    setTimeout(() => {
-        thankYouPopup.style.opacity = 0;
-    }, 2500);
-
-    if(money < -200){
-        window.alert("blud go sell your organs at this point, go get a job you broke ahh bum loser.")
-
-    } else if(money < 0){
-        window.alert("yo blud be carful you are in debt try to not go into more debt.")
-    } else{
-        money = money - 1;
-    }
-
-})
-
 window.onload = function(){
         const sizeS = document.getElementById("S");
         const sizeM = document.getElementById("M");
@@ -169,3 +93,33 @@ window.onload = function(){
 
 
     }
+
+    const jacketPrice = 79.99;
+let money = 700;
+const balanceDisplay = document.getElementById("balanceContainer");
+const cartBtn = document.querySelector(".cartBtn");
+const thankYouPopup = document.getElementById("thankYouPopup");
+
+cartBtn.addEventListener("click", function(){
+    money -= jacketPrice;
+     window.alert("Item added to cart!")
+
+    balanceDisplay.textContent = "Balance: £" + money.toFixed(2);
+
+
+    thankYouPopup.style.opacity = 1;
+
+    setTimeout(() => {
+        thankYouPopup.style.opacity = 0;
+    }, 2500);
+
+    if(money < -200){
+        window.alert("blud go sell your organs at this point, go get a job you broke ahh bum loser.")
+
+    } else if(money < 0){
+        window.alert("yo blud be carful you are in debt try to not go into more debt.")
+    } else{
+        money = money - 1;
+    }
+
+});
